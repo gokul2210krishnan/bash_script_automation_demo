12d46eda740d6fe6758748e5fadb198bf1b6b36d
gokul2210krishnan <gokul2210krishnan@gmail.com>
2022-07-28 19:17:38 +0530

    sample commit 15291

--- fishyly_update_repo.sh
+++ fishyly_update_repo.sh
@@ -2,8 +2,7 @@
 # declare String variable
 STRING="Hello World$RANDOM"
 #print variable on screen
-echo $STRING >> bash_script_automation_demo/changable_file/demo_1.text
-cd bash_script_automation_demo
+echo $STRING >> changable_file/demo_1.text
 git add .
 git commit -m "sample commit $RANDOM"
 git push origin main
